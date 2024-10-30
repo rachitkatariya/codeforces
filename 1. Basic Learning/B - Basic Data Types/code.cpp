79f234e16423a11e1e62101caba084d7faa8b204
rachitkatariyaa <rachitkumar2953@gmail.com>
2024-10-30 17:17:54 +0530

    initial commit

---
+++ 1. Basic Learning/B - Basic Data Types/code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int, long long, char, float and double r
+
+    int n;
+    long long ll;
+    char c;
+    float f;
+    double d;
+
+    cin >> n >> ll >> c >> f >> d;
+
+    cout << n << endl;
+    cout << ll << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d << endl;
+}
